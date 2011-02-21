9e4cad72eaea546ad93ddc97a3770e7326e48a9e
Jacob Lewallen <jlewallen@gmail.com>
2011-02-20 23:08:54 -0800

    Setting jenkins prefix, fix a weird binding issue.

--- modules/jenkins/manifests/jenkins.pp
+++ modules/jenkins/manifests/jenkins.pp
@@ -8,12 +8,24 @@ class jenkins {
     require => Exec["install-jenkins-key"]
   }
 
+  file { "/etc/sysconfig": ensure => directory }
+
+  file { "/etc/sysconfig/jenkins":
+    content => 'JENKINS_JAVA_OPTIONS="-Djava.awt.headless=true -Dnet.mdns.interface=0.0.0.0 "',
+    require => File["/etc/sysconfig"]
+  }
+
+  file { "/etc/default/jenkins":
+    content => template("jenkins/jenkins.erb"),
+    notify => Service["jenkins"]
+  }
+
   service { 'jenkins':
     provider => debian,
     ensure => running,
     enable => true,
     hasstatus => false,
     hasrestart => true,
-    require => Package['jenkins'],
+    require => [ Package['jenkins'], File["/etc/sysconfig/jenkins"], File["/etc/default/jenkins"] ],
   }
 }
